380fe0c5ca56be399a772b9752a9fce56c55498d
Kavindu Lakmal <87576012+KavinduLakmal2000@users.noreply.github.com>
2024-10-14 23:38:09 +0530

    Update Robot_MCU.ino

--- Code/Robot_MCU/Robot_MCU.ino
+++ Code/Robot_MCU/Robot_MCU.ino
@@ -1,15 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "SLT_Fiber_Optic";           
+const char* password = "Life1Mal7i";   
+const char* targetIP = "192.168.1.11";  
+
+WiFiClient client;  
+
 void setup() {
-  // Initialize Serial communication with Arduino Pro Mini
+ 
   Serial.begin(9600);          
-  Serial.setTimeout(10);  // Set timeout for serial reading
+  Serial.setTimeout(10); 
+
+  WiFi.begin(ssid, password);  
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi!");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
 }
 
 void loop() {
-  // Send "ON" command to Arduino Pro Mini
-  Serial.println("ON");
-  delay(1000); // Wait for 1 second
 
-  // Send "OFF" command to Arduino Pro Mini
+  Serial.println("ON");
+  delay(1000); 
+
   Serial.println("OFF");
-  delay(1000); // Wait for 1 second
+  delay(1000); 
+
+
+
+  sendHTTPRequest("/cut230On");
+  delay(1000);  
+  
+  sendHTTPRequest("/cut230Off");
+  delay(1000);  
+
+  sendHTTPRequest("/ArlamOn");
+  delay(1000);  
+
+  sendHTTPRequest("/ArlamOff");
+  delay(1000);  
+
+}
+
+void sendHTTPRequest(String command) {
+  if (WiFi.status() == WL_CONNECTED) {  
+    HTTPClient http;
+    String url = "http://" + String(targetIP) + command;  
+
+    http.begin(client, url); 
+    int httpCode = http.GET(); 
+
+    if (httpCode > 0) {
+      String payload = http.getString();  
+      Serial.println(payload);  
+    } else {
+      Serial.println("Error in HTTP request");
+    }
+    http.end();  
+  } else {
+    Serial.println("WiFi not connected");
+  }
 }
