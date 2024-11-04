bf0724ad96b86242f55fb67fe459b391b76f2e2c
Kavindu Lakmal <87576012+KavinduLakmal2000@users.noreply.github.com>
2024-11-04 12:35:32 +0530

    Update Robot_cutoff_Unit_NodeMcu.ino

--- Code/Robot_cutoff_Unit_NodeMcu/Robot_cutoff_Unit_NodeMcu.ino
+++ Code/Robot_cutoff_Unit_NodeMcu/Robot_cutoff_Unit_NodeMcu.ino
@@ -1,7 +1,13 @@
 //------------------------------------------receiver
 
+// D5 green
+// D6 Red
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+
+#define greenLed D5
+#define redLed D6
 
 const char* ssid = "SLT_Fiber_Optic";        
 const char* password = "Life1Mal7i"; 
@@ -9,35 +15,57 @@ const char* password = "Life1Mal7i";
 ESP8266WebServer server(80);   
 
 const int MainCutOff = D2;
-const int Arlam = D3;                  
+const int Arlam = D3;   
+int count = 0;               
 
 void setup() {
 
   pinMode(MainCutOff, OUTPUT);
   pinMode(Arlam, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
 
   digitalWrite(MainCutOff, LOW);  
   digitalWrite(Arlam, LOW);  
 
   Serial.begin(9600);
-  
   WiFi.begin(ssid, password);
+  unsigned long startAttemptTime = millis();
+  bool connected = false;
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi...");
+    digitalWrite(greenLed, HIGH);
+    delay(250);
+    digitalWrite(greenLed, LOW);
+    delay(250);
+    
+    if (millis() - startAttemptTime >= 10000) {
+      Serial.println("Failed to connect to WiFi.");
+      digitalWrite(redLed, HIGH);  
+      connected = false;
+      break;
+    }
   }
-  Serial.println("Connected to WiFi!");
-  Serial.println(WiFi.localIP());  
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected to WiFi!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    connected = true;
+    digitalWrite(greenLed, HIGH);  
+  }
 
   
   server.on("/cut230On", []() {
     digitalWrite(MainCutOff, HIGH);     
     server.send(200, "text/plain", "ON");
+    digitalWrite(redLed, HIGH);  
     Serial.println("Power is cut!");
   });
 
   server.on("/cut230Off", []() {
-    digitalWrite(MainCutOff, LOW);    
+    digitalWrite(MainCutOff, LOW); 
+    digitalWrite(redLed, LOW);     
     server.send(200, "text/plain", "OFF");
     Serial.println("turned off the power cut switch!");
   });
@@ -57,8 +85,28 @@ void setup() {
 
   server.begin();
   Serial.println("Server started");
+  delay(2000);
+  digitalWrite(greenLed, LOW);
 }
 
 void loop() {
-  server.handleClient();  
+  server.handleClient();
+  Indicator_Led(100000, 110000);
+}
+
+
+void Indicator_Led(int C_start, int C_end){
+
+if (count == C_start){
+  digitalWrite(greenLed, HIGH);
+}
+
+else if (count == C_end){
+  digitalWrite(greenLed, LOW);
+  count = 0;
+}
+
+
+count++;
+
 }
